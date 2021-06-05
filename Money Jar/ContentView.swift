a2e3a9193949358cb97691ee1097ca21a948cfc9
Holy-Elie Scaide <skydhash@gmail.com>
2021-06-04 21:23:36 -0400

    Initial Commit

---
+++ Money Jar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Money Jar
+//
+//  Created by Holy-Elie Scaide on 6/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Money Jar/Money_JarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Money_JarApp.swift
+//  Money Jar
+//
+//  Created by Holy-Elie Scaide on 6/4/21.
+//
+
+import SwiftUI
+
+@main
+struct Money_JarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
